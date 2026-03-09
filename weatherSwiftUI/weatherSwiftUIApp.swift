8b7cfbb6e1b897237dacff5455b03acdfc276f09
Mikhail <maestrocrash@gmail.com>
2026-03-09 20:59:37 +0300

    Initial Commit

---
+++ weatherSwiftUI/weatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherSwiftUIApp.swift
+//  weatherSwiftUI
+//
+//  Created by MICHAIL SHAKHVOROSTOV on 09.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct weatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
